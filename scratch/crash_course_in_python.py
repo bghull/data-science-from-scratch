fd655df78c2f940072ad09085ebe951651ff3feb
bghull <35785950+bghull@users.noreply.github.com>
2020-09-10 13:38:38 -0400

    Deleted important stuff

--- scratch/crash_course_in_python.py
+++ scratch/crash_course_in_python.py
@@ -1,27 +1,6 @@
 
 """
-This is just code for the introduction to Python.
-It also won't be used anywhere else in the book.
-"""
-# type: ignore
-
-# The pound sign marks the start of a comment. Python itself
-# ignores the comments, but they're helpful for anyone reading the code.
-for i in [1, 2, 3, 4, 5]:
-    print(i)                    # first line in "for i" block
-    for j in [1, 2, 3, 4, 5]:
-        print(j)                # first line in "for j" block
-        print(i + j)            # last line in "for j" block
-    print(i)                    # last line in "for i" block
-print("done looping")
-
-long_winded_computation = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 +
-                           13 + 14 + 15 + 16 + 17 + 18 + 19 + 20)
-
-list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-easier_to_read_list_of_lists = [[1, 2, 3],
-                                [4, 5, 6],
+This is just code for the introduc
                                 [7, 8, 9]]
 
 two_plus_three = 2 + \
